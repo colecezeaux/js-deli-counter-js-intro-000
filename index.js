7b44b88b414bd39aa371c0df29928596200bc90f
Cole Cezeaux <colecezeaux@gmail.com>
2020-09-24 21:54:10 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ function nowServing(line) {
   if(line.length == 0) {
     return "Theere is nobody waiting to be served!"
   } else {
-    return `Currently serving ${line[i]}.`
+    return `Currently serving ${line[0]}.`
   }
   line.shift()
 }
