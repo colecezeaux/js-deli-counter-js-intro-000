e09edb3d060dfc2381f4c1b562f9608126b22f68
Cole Cezeaux <colecezeaux@gmail.com>
2020-09-24 21:49:42 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,3 +2,11 @@ function takeANumber(line, name) {
   line.push(name)
   return `Welcome, ${name}. You are number ${line.length} in line.`
 }
+
+function nowServing(line) {
+  if(line.length = 0) {
+    return "The line is currently empty."
+  } else {
+    return `Now serving ${line.shift()}.`
+  }
+}
