2801fcfcef7bedf23ff01f364e22839c8ae3811d
Cole Cezeaux <colecezeaux@gmail.com>
2020-09-24 22:07:47 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -12,3 +12,16 @@ var n = []
   }
   return n
 }
+
+function currentLine(line) {
+var n = []
+  if(line.length == 0) {
+    n.push("The line is currently empty.")
+  } else {
+    let line = `1. ${name}, `
+    for (let i = 0; i < line.length; i++) {
+     n.push(`The line is currently: ${line[i]}`);
+      }
+    }
+    return n
+}
