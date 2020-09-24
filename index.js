6ceaee1eb8a9aa3d23e8de7a31b57b770df3ca53
Cole Cezeaux <colecezeaux@gmail.com>
2020-09-24 21:54:05 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ function takeANumber(line, name) {
 
 function nowServing(line) {
   if(line.length == 0) {
-    return "The line is currently empty."
+    return "Theere is nobody waiting to be served!"
   } else {
     return `Currently serving ${line[i]}.`
   }
