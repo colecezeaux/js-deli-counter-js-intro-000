6943bba924f86d819a7078ebdb757d63ef60f06b
Cole Cezeaux <colecezeaux@gmail.com>
2020-09-24 22:20:11 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ var n = []
   if(line.length == 0) {
     return "The line is currently empty."
   } else {
-    let num = []
+    let i = []
     let order = `1. ${line[0]}`
     for (let i = 0; i < line.length; i++) {
       order = order + `, ${i+1}. ${line[i]}`
